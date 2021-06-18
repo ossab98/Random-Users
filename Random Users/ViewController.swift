d0e5ed4c445e5c7c862247db7fe2cc9b8ce3d871
Ossama Abdelwahab <ossab98@gmail.com>
2021-06-18 15:18:55 +0200

    Initial commit

---
+++ Random Users/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Random Users
+//
+//  Created by Ossama Abdelwahab on 18/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
